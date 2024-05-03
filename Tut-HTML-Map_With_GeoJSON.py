edda01b8df543f21e0b581b26a9f599c78d34bb9
Garcia-INPE <99909390+Garcia-INPE@users.noreply.github.com>
2024-05-03 16:05:49 -0300

    Update and rename Tut-MapWithGeoJSONInHTML.py to Tut-HTML-Map_With_GeoJSON.py

--- Tut-HTML-Map_With_GeoJSON.py
+++ Tut-HTML-Map_With_GeoJSON.py
@@ -1,6 +1,6 @@
 import folium
 import folium.map
-json_file = "Linhas_200_milhas.geojson"
+json_file = "filename.geojson"
 m = folium.Map()
 folium.GeoJson(json_file).add_to(m)
-m.save('br_map.html')
+m.save('filename.html')
